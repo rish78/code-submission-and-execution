cf553edff5850da234a1ddadbb5da1d3b14538ed
Rishabh <rishabhkr.073@gmail.com>
2024-03-21 20:06:40 +0530

    adding hosted redis server

--- configs/cache.js
+++ configs/cache.js
@@ -1,5 +1,5 @@
 const Redis = require("ioredis");
-const redisUri = "rediss://default:AVNS_oqU1G-kTWaa0k2jEBBv@redis-2cb55bd3-rishabh02cv-8d12.a.aivencloud.com:12133"
+const redisUri = process.env.REDIS_URI;
 const redisClient = new Redis(redisUri);
 
 module.exports = redisClient;
